fde277cc339690bb62abbaf16fb80f0d37c14afc
vtdnguyen <89002918+vtdnguyen@users.noreply.github.com>
2024-05-05 22:35:08 +0700

    modify dnguyen sched parts

--- src/queue.c
+++ src/queue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
-
+#define FALSE 0;
 int empty(struct queue_t *q)
 {
         if (q == NULL)
@@ -12,18 +12,22 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
         /* TODO: put a new process to queue [q] */
+        int proc_queue_is_having = q->size;
         if (q == NULL)
         {
                 perror("Queue is NULL !\n");
                 exit(1);
         }
-        if (q->size == MAX_QUEUE_SIZE)
+        else if (proc_queue_is_having >= MAX_QUEUE_SIZE)
         {
                 perror("Queue is full !\n");
                 exit(1);
         }
-        q->proc[q->size] = proc;
-        q->size++;
+        else if (proc_queue_is_having < MAX_QUEUE_SIZE)
+        {
+                q->proc[proc_queue_is_having] = proc;
+                q->size = proc_queue_is_having + 1;
+        }
 }
 
 struct pcb_t *dequeue(struct queue_t *q)
@@ -39,6 +43,18 @@ struct pcb_t *dequeue(struct queue_t *q)
         else
         {
                 struct pcb_t *temp = q->proc[0];
+        // #ifdef FALSE
+        //         for (int i = 0; i < proc_queue_is_having - 1; i++)
+        //         {
+        //                 if (q->proc[i] && q->proc[i + 1])
+        //                 {
+        //                         q->proc[i] = q->proc[i + 1];
+        //                 }
+        //         }
+        //         q->proc[proc_queue_is_having - 1] = NULL;
+        //         q->size--;
+        //         return temp;
+        // #else
                 uint32_t flag_prio = temp->priority;
                 int pos = 0;
                 for (int i = 0; i < proc_queue_is_having; i++)
@@ -61,7 +77,7 @@ struct pcb_t *dequeue(struct queue_t *q)
                 q->proc[proc_queue_is_having - 1] = NULL;
                 q->size--;
                 return temp;
-        }
+        //#endif
 
-        return NULL;
-}
+        }    
+}   
--- src/sched.c
+++ src/sched.c
@@ -57,7 +57,11 @@ struct pcb_t *get_mlq_proc(void)
 	pthread_mutex_lock(&queue_lock);
 	for (int i = 0; i < MAX_PRIO; i++)
 	{
-		if (mlq_ready_queue[i].size > 0 && slot[i] > 0)
+		if (empty(&mlq_ready_queue[i]) || slot[i] == 0){
+			slot[i] = MAX_PRIO - 1;
+			continue;
+		}
+		else if (mlq_ready_queue[i].size > 0 && slot[i] > 0)
 		{
 
 			proc = dequeue(&mlq_ready_queue[i]);
